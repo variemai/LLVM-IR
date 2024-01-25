b82ab71c2e3bfbc33a399a2d9dda00fccafdf58b
Ioannis Vardas <vardas@par.tuwien.ac.at>
2024-01-25 18:22:41 +0100

    Try on linux

---
+++ Hello_Pass/function_pass.h
@@ -0,0 +1,15 @@
+#ifndef LLVM_FUNPASS_H
+#define LLVM_FUNPASS_H
+
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+class HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+};
+
+} // namespace llvm
+
+#endif // LLVM_FUNPASS_H
---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int add(int a){
+	return a+a;
+}
+
+int main() {
+  printf("hello world\n");
+  return 0;
+}
